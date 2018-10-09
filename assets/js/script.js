0af1bab41acb9b41c6c46fc7ecee67c08f89cf2b
Stiouf <stephane.quentin@live.fr>
2018-10-09 12:04:24 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 function ex8(){
-  var ag = document.getElementById("age").value; // récupération de la première variable que l'on a tapé dans la page html
-  if(isNaN(ag) || ag==''){ // si age n'est pas un chiffre ou champ vide
-    alert('En chiffre !') // affichage d'un message d'alerte
-  }else{ //sinon on continue
-    if (ag >= 18) {
-      alert("Vous êtes majeur :)"); //Si ag (l'age) est supérieur ou égal à 18 affichage du message "Vous êtes majeur"
-    } else {
-      alert("Vous êtes mineur :("); // Sinon affichage du message "Vous êtes mineur"
-    }
+  var ag = document.getElementById("age").value; // récupération de la valeur saisie dans l'input dans la variable ag
+  if(isNaN(ag) || ag < 1){ // si age n'est pas un chiffre ou champ vide
+  alert('En chiffre !') // affichage d'un message d'alerte
+}else{ //sinon on continue
+  if (ag >= 18) {
+    alert("Vous êtes majeur :)"); //Si ag (l'age) est supérieur ou égal à 18 affichage du message "Vous êtes majeur"
+  } else {
+    alert("Vous êtes mineur :("); // Sinon affichage du message "Vous êtes mineur"
+  }
 }
 }
